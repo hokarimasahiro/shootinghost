d786347611232906be55f9da47a049dc6805645c
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2025-04-03 15:06:18 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-function respons処理 () {
-    if (receivedCommand[3] == "HIT") {
+function responsProc () {
+    if (receivedCommand[3].charAt(0) == "H") {
         point += 1
         watchfont.showNumber2(point)
     }
@@ -12,18 +12,19 @@ radio.onReceivedString(function (receivedString) {
     serial.writeLine(receivedString)
     receivedCommand = receivedString.split(",")
     if (receivedCommand[0] == "CQ") {
-        CQ受信処理()
+        CQreceiveProc()
     } else if (receivedCommand[0] == control.deviceName()) {
-        respons処理()
+        responsProc()
     }
 })
-function CQ受信処理 () {
+function CQreceiveProc () {
     radio.sendString("" + receivedCommand[1] + "," + control.deviceName() + "," + convertToText(radioGroup))
     if (targetList.indexOf(receivedCommand[1]) == -1) {
         targetList.push(receivedCommand[1])
         serial.writeLine("target add " + receivedCommand[1])
     }
 }
+let sendStrings = ""
 let point = 0
 let receivedCommand: string[] = []
 let radioGroup = 0
@@ -39,7 +40,8 @@ watchfont.showNumber2(radioGroup)
 radio.setGroup(0)
 radio.setTransmitPower(7)
 basic.forever(function () {
-    radio.sendString("" + targetList._pickRandom() + "," + control.deviceName() + "," + "START" + "," + convertToText(waitTime) + "," + convertToText(dataValidTime))
-    basic.pause(waitTime)
+    sendStrings = "" + targetList._pickRandom() + "," + "S" + "," + convertToText(waitTime) + "," + convertToText(dataValidTime)
+    radio.sendString(sendStrings)
+    serial.writeLine(sendStrings)
     basic.pause(waitTime)
 })
